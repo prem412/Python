31c7b90e79312efc3e27993a05c72a3578e16469
Prem Sidapara <pprem1644@gmail.com>
2025-07-12 23:11:38 +0530

    Operators and Expressions

---
+++ Operators_Expressions.py
@@ -0,0 +1,47 @@
+#Arithmetic operators
+a = 10
+b = 20
+print(a + b) #Addition
+print(a - b) #Subtraction
+print(a * b) #Multiplication
+print(a / b) #Division
+print(a // b) #Floor Division
+print(a % b) #Modulus
+print(a ** b) #Exponentiation
+
+
+
+
+
+
+#comparison operators
+x = 5
+y = 20
+print(x == y)  # equal  
+print(x != y)  # not equal
+print(x > y)   # greater than
+print(x < y)   # less than
+print(x >= y)  # greater than or equal to
+print(x <= y)  # less than or equal to
+
+
+
+
+
+
+
+
+#logical operators
+P = True
+L = False
+print(P and L)  # Logical AND
+print(P or L)   # Logical OR
+print(not P)    # Logical NOT
+
+m = 10
+n = 20
+print(m > 5 and n < 30)  # both conditions are true
+print(m < 5 or n > 15)   # at least one condition is true
+print(not (m == n))       # m is not equal to n   
+print(not (m == 10 ))      # m is not equal to 10
+
